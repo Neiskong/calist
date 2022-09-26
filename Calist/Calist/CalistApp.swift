2a1e739ca920330b9c0f7969c333f597741c434d
jeohong <jeohong59@gmail.com>
2022-09-26 21:42:34 +0900

    project init

---
+++ Calist/Calist/CalistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalistApp.swift
+//  Calist
+//
+//  Created by Hong jeongmin on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CalistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
